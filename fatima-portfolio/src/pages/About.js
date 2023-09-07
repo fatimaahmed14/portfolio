be904bc2a4bdc9cbef88be991b5258cc2aec8ceb
fatima <fxtimaahmed14@gmail.com>
2023-09-07 11:30:31 +0100

    styling changes on tech stack => thinking about backgrounds

--- fatima-portfolio/src/pages/About.js
+++ fatima-portfolio/src/pages/About.js
@@ -4,12 +4,13 @@ function About() {
   return (
     <div className="about-me">
       <div className="text">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris laoreet
-        scelerisque purus feugiat condimentum. In odio nulla, blandit ut
-        bibendum et, sollicitudin a ex. Fusce id erat id neque vestibulum
-        luctus. Suspendisse imperdiet tellus lorem, nec hendrerit lorem
-        hendrerit eu. Aliquam sagittis purus augue, quis tempus dolor placerat
-        ut. Maecenas euismod vehicula dolor, vel cursus lacus dictum et.
+        I am a Junior Software Developer - I say junior becasue I started my
+        coding journey last August. Regarless, I can truly say I have learnt
+        ALOT over the course of this past year.
+        <br></br>
+        The technolgies I use, cover both back-end and front-end. I am a lover
+        of beautiful + well-executed UX designs and I aim to contiue to create
+        beautiful intuative products as I go on in my coding journey.
       </div>
     </div>
   );
